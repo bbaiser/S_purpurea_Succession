460dc4f282f24602dadaa77f1846dd307929bf6e
Baiser <bbaiser@ufl.edu>
2024-11-18 15:21:10 -0500

    fixed paths

--- Code/SES_PD_analysis.R
+++ Code/SES_PD_analysis.R
@@ -2,7 +2,7 @@
 
 
 ####set_up####
-#Install and load relevant pacakges 
+#Install and load relevant packages 
 library(tidyverse)
 library(dplyr)
 library(ape)
@@ -10,16 +10,16 @@ library(vegan)
 library(picante)
 
 #call in data
-#the pitcher by ASV table (rowa are pitchers (n=515, columns are ASVs (n=3376))
-asv <- read.table ("Data/final/pitcher_plant_ASV_table.csv", sep= ",", check.names = F,row=1, header =T)
+#the pitcher by ASV table (rows are pitchers n=515, columns are ASVs n=3376)
+asv <- read.table ("Data/pitcher_plant_ASV_table.csv", sep= ",", check.names = F,row=1, header =T)
 dim(asv)
 
 #meta is the covariate data associated with each pitcher (n=515)
-meta <- read.table("Data/final/pitcher_plant_meta_table.csv", sep= ",", header=T)
+meta <- read.table("Data/pitcher_plant_meta_table.csv", sep= ",", header=T)
 dim(meta)
 
 #phylogenetic tree for the ASVs
-asv16s.tree <- read.tree("Data/final/pitcher_plant_tree.nwk")
+asv16s.tree <- read.tree("Data/pitcher_plant_tree.nwk")
 
 #ses.pd run null model for each site separately
 ####subset for QC####
@@ -33,7 +33,7 @@ pdTree<-prune.sample(asv2, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asv2, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_QC.csv")
+#write.csv(pitcher_pd, "Data/PD_QC.csv")
 
 ####subset for FL####
 asvFL<-as.data.frame(asv[grep('^FL', rownames(asv)),])%>%
@@ -46,7 +46,7 @@ pdTree<-prune.sample(asvFL, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asvFL, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_FL.csv")
+#write.csv(pitcher_pd, "Data/PD_FL.csv")
 
 ####subset for GA####
 asvGA<-as.data.frame(asv[grep('^GA', rownames(asv)),])%>%
@@ -59,7 +59,7 @@ pdTree<-prune.sample(asvGA, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asvGA, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_GA.csv")
+#write.csv(pitcher_pd, "Data/PD_GA.csv")
 
 ####subset for IN####
 asvIN<-as.data.frame(asv[grep('^IN', rownames(asv)),])%>%
@@ -72,7 +72,7 @@ pdTree<-prune.sample(asvIN, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asvIN, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_IN.csv")
+#write.csv(pitcher_pd, "Data/PD_IN.csv")
 
 ####subset for MA####
 asvMA<-as.data.frame(asv[grep('^MA', rownames(asv)),])%>%
@@ -85,7 +85,7 @@ pdTree<-prune.sample(asvMA, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asvMA, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_MA.csv")
+#write.csv(pitcher_pd, "Data/PD_MA.csv")
 
 ####subset for NC ####
 asvNC<-as.data.frame(asv[grep('^NC', rownames(asv)),])%>%
@@ -98,7 +98,7 @@ pdTree<-prune.sample(asvNC, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asvNC, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_NC.csv")
+#write.csv(pitcher_pd, "Data/PD_NC.csv")
 
 ####subset for WI ####
 asvWI<-as.data.frame(asv[grep('^WI', rownames(asv)),])%>%
@@ -111,4 +111,4 @@ pdTree<-prune.sample(asvWI, asv16s.tree)
 #calculate PD and SESPD for site
 pitcher_pd<-ses.pd(asvWI, pdTree, null.model="taxa.labels", runs=999)
 
-#write.csv(pitcher_pd, "Data/final/PD_WI.csv")
+#write.csv(pitcher_pd, "Data/PD_WI.csv")
